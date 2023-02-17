297ec143e3054a935da4d537ee99061bcf271fa9
Joe Spoelstra <jjspoels@gmail.com>
2023-02-17 12:41:16 -0500

    daily codewars

---
+++ 8kyu pick a set of first elements.js
@@ -0,0 +1,12 @@
+function first(arr, n=1) {
+    return arr.slice(0, n)
+  }
+  
+  
+  //parameters: given an array and a number of elements in the array to isolate.
+  //return the isolated elements as a new array
+  //example shown
+  //pseudo:
+    //slice the array, starting at index 0, goinmg until the number
+    //return a new array from the sliced one
+    //n should default to one
